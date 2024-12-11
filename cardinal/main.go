1e48d29f17e04eec3d618896c559785c77ceff1f
Ryandi Tjia <ryandi@argus.gg>
2024-12-12 01:01:48 +0700

    feat: remove unused example code

--- cardinal/main.go
+++ cardinal/main.go
@@ -5,11 +5,6 @@ import (
 
 	"github.com/rs/zerolog/log"
 	"pkg.world.dev/world-engine/cardinal"
-
-	"argus-exercise-1-backend/component"
-	"argus-exercise-1-backend/msg"
-	"argus-exercise-1-backend/query"
-	"argus-exercise-1-backend/system"
 )
 
 func main() {
@@ -28,37 +23,23 @@ func main() {
 func MustInitWorld(w *cardinal.World) {
 	// Register components
 	// NOTE: You must register your components here for it to be accessible.
-	Must(
-		cardinal.RegisterComponent[component.Player](w),
-		cardinal.RegisterComponent[component.Health](w),
-	)
+	// Must()
 
 	// Register messages (user action)
 	// NOTE: You must register your transactions here for it to be executed.
-	Must(
-		cardinal.RegisterMessage[msg.CreatePlayerMsg, msg.CreatePlayerResult](w, "create-player"),
-		cardinal.RegisterMessage[msg.AttackPlayerMsg, msg.AttackPlayerMsgReply](w, "attack-player"),
-	)
+	// Must()
 
 	// Register queries
 	// NOTE: You must register your queries here for it to be accessible.
-	Must(
-		cardinal.RegisterQuery[query.PlayerHealthRequest, query.PlayerHealthResponse](w, "player-health", query.PlayerHealth),
-	)
+	// Must()
 
 	// Each system executes deterministically in the order they are added.
 	// This is a neat feature that can be strategically used for systems that depends on the order of execution.
 	// For example, you may want to run the attack system before the regen system
 	// so that the player's HP is subtracted (and player killed if it reaches 0) before HP is regenerated.
-	Must(cardinal.RegisterSystems(w,
-		system.AttackSystem,
-		system.RegenSystem,
-		system.PlayerSpawnerSystem,
-	))
+	// Must(cardinal.RegisterSystems(w))
 
-	Must(cardinal.RegisterInitSystems(w,
-		system.SpawnDefaultPlayersSystem,
-	))
+	// Must(cardinal.RegisterInitSystems(w))
 }
 
 func Must(err ...error) {
